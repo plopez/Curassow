411b95672cb0486b1733043019f9a9283364f91c
Pablo Lopez <plopez@xebia.fr>
2016-09-10 00:30:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
   name: "Curassow",
-  testDependencies: [
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 6),
-  ],
   dependencies: [
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 3),
     .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 3),
